0fde9b86a0ec1afd23695072f46a06ce4858ec43
Nakum Nikunj <168266686+Zzccjx@users.noreply.github.com>
2026-08-04 21:07:06 +0530

    Create string.py

---
+++ string.py
@@ -0,0 +1,31 @@
+text = "Python Programming"
+
+# Indexing
+print( text[0])
+print( text[-1])
+
+# Slicing
+print( text[:6])
+print( text[7:])
+print( text[::-1])
+print( text[2:6])
+print( text[0:])
+print( text[-1:])
+print( text[-1:4])
+
+# Formatting
+name = "Nikunj"
+age = 20
+
+print(f"My name is {name} and I am {age} years old.")
+
+# Built-in Functions
+print(text.upper())
+print(text.lower())
+print(text.title())
+print(text.replace("Python", "Java"))
+print(text.count("m"))
+print(text.find("Pro"))
+print(text.startswith("Python"))
+print(text.endswith("ing"))
+print(text.split())
