13056fe405ffc2fc0599b1e67c775d8a43650c50
Nakum Nikunj <168266686+Zzccjx@users.noreply.github.com>
2026-08-04 21:09:06 +0530

    Create tuple.py

---
+++ tuple.py
@@ -0,0 +1,31 @@
+
+
+student = ("Nikunj", 20, "Rajkot", "MCA")
+
+print( student)
+print( student[0])
+print( student[1:3])
+print( len(student))
+numbers = (1,2,2,3,2)
+print( numbers.count(2))
+print( numbers.index(3))
+
+
+
+A = {10,20,30}
+
+B = {20,30,40}
+
+print( A)
+print( B)
+
+A.add(50)
+print( A)
+
+A.remove(10)
+print( A)
+
+print( A | B)
+print( A & B)
+print( A - B)
+print( 30 in A)
