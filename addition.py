35c9286fcc4fe4ec0a545f6b752eea0da77bd26b
Nakum Nikunj <168266686+Zzccjx@users.noreply.github.com>
2026-08-04 21:05:53 +0530

    Create addition.py

---
+++ addition.py
@@ -0,0 +1,24 @@
+a = int(input("Enter the number  : "))
+b = int(input("Enter the number  : "))
+
+# Arithmetic Operation
+print("Addition =", a + b)
+print("Subtraction =", a - b)
+print("Multiplication =", a * b)
+print("Division =", a / b)
+print("Floor Division =", a // b)
+print("Modulus =", a % b)
+print("Power =", a ** b)
+
+# Relational Operation
+print("Equal:", a == b)
+print("Not Equal:", a != b)
+print("Greater:", a > b)
+print("Less:", a < b)
+print("Greater or Equal:", a >= b)
+print("Less or Equal:", a <= b)
+
+# Logical Operation
+print("AND:", a < b and b > a)
+print("OR:", a > b or b > a)
+print("NOT:", not(a < b))
