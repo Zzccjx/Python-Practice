b05b048f7f78dda56318655cdb61f65c45da6682
Nakum Nikunj <168266686+Zzccjx@users.noreply.github.com>
2026-08-11 12:18:43 +0530

    Create positive_negetive.py

---
+++ positive_negetive.py
@@ -0,0 +1,11 @@
+num = int(input(" Enter a number : "))
+
+if num >= 0:
+    if num == 0:
+        print("the number is zero")
+
+    else:
+        print("the number is positive")
+
+else:
+    print("the number is negetive")
