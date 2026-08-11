c55900b613ece90c1eb3d32bcad7ad66282ff69e
Nakum Nikunj <168266686+Zzccjx@users.noreply.github.com>
2026-08-11 12:17:46 +0530

    Create conditional_statement.py

---
+++ conditional_statement.py
@@ -0,0 +1,25 @@
+marks = int(input("Enter your marks  : "))
+
+if marks >= 95:
+    print("grade A++")
+    
+elif marks >= 90:
+    print("grade A+")
+    
+elif marks >= 80:
+    print("grade A")
+    
+elif marks >= 70:
+    print("grade B")
+    
+elif marks >= 60:
+    print("grade C")
+    
+elif marks >= 50:
+    print("grade D")
+    
+elif marks >= 40:
+    print("grade E")
+    
+else:
+    print("Grade: F (Fail)")
