3030d65675ecd5ef2a58238b73a8915669c8c4c2
Nakum Nikunj <168266686+Zzccjx@users.noreply.github.com>
2026-08-12 22:34:03 +0530

    Create iterable.py

---
+++ iterable.py
@@ -0,0 +1,16 @@
+
+numbers = [10, 20, 30, 40, 50]
+
+print( numbers)
+
+
+iterator = iter(numbers)
+
+print("\nUsing iterator:")
+
+
+print(next(iterator))
+print(next(iterator))
+print(next(iterator))
+print(next(iterator))
+print(next(iterator))
