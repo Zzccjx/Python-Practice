02fb88ada485e9722950b2d4f4e7dda9cfaa893f
Nakum Nikunj <168266686+Zzccjx@users.noreply.github.com>
2026-08-12 12:21:12 +0530

    Create iterate_list.py

---
+++ iterate_list.py
@@ -0,0 +1,20 @@
+fruits = ["Apple", "Mango", "Banana"]
+
+print("LIST :")
+for fruit in fruits:
+    print(fruit)
+
+name = "PYTHON"
+
+print("\nSTRING")
+for ch in name:
+    print(ch)
+
+student ={
+    "Name" : "Tom",
+    "Age"    : "20",
+    "course" : "MCA"
+    }
+print("\nDICTIONARY : ")
+for key, value in student.items():
+    print(key, ":", value)
