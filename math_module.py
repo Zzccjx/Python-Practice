5c12115598f9f0884478eb7e855eacd9728da6ab
Nakum Nikunj <168266686+Zzccjx@users.noreply.github.com>
2026-09-23 21:44:50 +0530

    Create math_module.py

---
+++ math_module.py
@@ -0,0 +1,10 @@
+import math
+
+number = 25
+
+print("Square root:", math.sqrt(number))
+print("Power:", math.pow(2, 3))
+print("Factorial:", math.factorial(5))
+print("Ceiling:", math.ceil(4.3))
+print("Floor:", math.floor(4.8))
+print("Value of PI:", math.pi)
