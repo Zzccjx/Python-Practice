0c84798a50cdfc05d8b822ddcc23720373cea414
Nakum Nikunj <168266686+Zzccjx@users.noreply.github.com>
2026-09-23 21:45:45 +0530

    Create random_module.py

---
+++ random_module.py
@@ -0,0 +1,13 @@
+import random
+
+number = random.randint(1, 100)
+
+print("Random number:", number)
+
+decimal = random.random()
+
+print("Random decimal:", decimal)
+
+names = ["Rahul", "Amit", "Neha", "Priya"]
+
+print("Random name:", random.choice(names))
