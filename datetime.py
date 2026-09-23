c3410753f5d4a96c45f218c4acc2ad79d242a01c
Nakum Nikunj <168266686+Zzccjx@users.noreply.github.com>
2026-09-23 21:46:23 +0530

    Create datetime.py

---
+++ datetime.py
@@ -0,0 +1,7 @@
+from datetime import datetime
+
+current_datetime = datetime.now()
+
+print("Current Date and Time:", current_datetime)
+print("Current Date:", current_datetime.date())
+print("Current Time:", current_datetime.time())
