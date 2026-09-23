f7570551c9982baad1dac7f7c5ca87c09b2638c5
Nakum Nikunj <168266686+Zzccjx@users.noreply.github.com>
2026-09-23 21:43:03 +0530

    Create Mechanisms.py

---
+++ Mechanisms.py
@@ -0,0 +1,18 @@
+
+import math
+
+print("Square root:", math.sqrt(25))
+
+
+from math import factorial
+
+print("Factorial:", factorial(5))
+
+import math as m
+
+print("Power:", m.pow(2, 3))
+
+from math import sin, cos
+
+print("Sin 0:", sin(0))
+print("Cos 0:", cos(0))
